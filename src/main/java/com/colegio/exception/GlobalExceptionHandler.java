3a582009625dbee4af08c8f86c0656f5c4eb6297
Jesus Almircar Castellanos Paez <jacastellanos@RUNT.LOC>
2024-11-18 23:35:19 -0500

    Se agregan logs en caso de expetions no controladas

--- src/main/java/com/colegio/exception/GlobalExceptionHandler.java
+++ src/main/java/com/colegio/exception/GlobalExceptionHandler.java
@@ -1,4 +1,5 @@
 package com.colegio.exception;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -6,12 +7,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        // Creamos un objeto de respuesta personalizada
+        log.error("ResourceNotFoundException: {}", ex.getMessage(), ex);
         ErrorDetails errorDetails = ErrorDetails
                 .builder()
                 .statusCode( HttpStatus.NOT_FOUND.value())
@@ -23,7 +25,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
-        // Manejo general de excepciones
+        log.error("Global Exception: {}", ex.getMessage(), ex);
         ErrorDetails errorDetails = ErrorDetails
                 .builder()
                 .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
